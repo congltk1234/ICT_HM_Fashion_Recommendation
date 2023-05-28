91a705b3d87caaa7d570ad7907980d4d8ba9229f
STC1702 <stc17022002@gmail.com>
2023-05-28 18:51:21 +0700

    Add infomations and reduce code

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 st.set_page_config(layout="wide", initial_sidebar_state='expanded')
+import io
+import requests
+import json
 import pandas as pd
 import numpy as np
 from PIL import Image
 import urllib.request
 import cv2
-import tensorflow as tf
 # download the stopwords from NLTK
 import nltk
 @st.cache_resource
@@ -14,17 +16,14 @@ def load_nltk():
     nltk.download('wordnet')
 load_nltk()
 
+import tensorflow as tf
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.layers import TextVectorization
+from sentence_transformers import SentenceTransformer
+
 from utils.image_func import *
 from utils.nlp_func import *
 from utils.process import *
-from tensorflow.keras.applications import EfficientNetB0
-from tensorflow.keras.layers import TextVectorization
-import io
-import requests
-import json
-from sentence_transformers import SentenceTransformer
-
-
 
 
 @st.cache_data
@@ -41,11 +40,13 @@ def load_embedding():
     return image_embeddings,sentence_embeddings
 image_embeddings,sentence_embeddings = load_embedding()
 
+
 @st.cache_resource
 def load_SentenceTransformer_model():
     sbert_model = SentenceTransformer('bert-base-nli-mean-tokens')
     return sbert_model
 sbert_model = load_SentenceTransformer_model()
+
 
 @st.cache_resource
 def load_EfficientNetB0_model():
@@ -105,6 +106,7 @@ def load_captioning_model():
 
 new_model, max_decoded_sentence_length, vectorization, index_lookup = load_captioning_model()
 
+
 def generate_caption(img, model):
     # Pass the image to the CNN
     img = tf.expand_dims(img, 0)
@@ -133,9 +135,12 @@ def url(item_id):
     url = 'https://www2.hm.com/en_us/productpage.0'+ str(item_id) +'.html'
     return url
 
+
 def main():
+######################### SideBar  ###########################################
     page_options = ["🔎 Find similar items",
-                    "👥 Customer Recommendations"]
+                    "👥 Customer Recommendations",
+                    "📌 More Infomations"]
     
     page_selection = st.sidebar.radio("Try", page_options)
     
@@ -143,10 +148,12 @@ def main():
                   'Text description embeddings are calculated using :red[**BERT-based Sentence-transformers model**]: It maps sentences & paragraphs to a 768 dimensional dense vector space']
 
 #########################################################################################
-################ Sector 1: Find Similar Items #########################################################################
+################ Sector 1: Find Similar Items ###########################################
+#########################################################################################
+  
     if page_selection == page_options[0]:
         best_n = 6
-        imgURL = st.sidebar.text_input('Image path', '')
+        imgURL = st.sidebar.text_input('Image path', 'https://media.githubusercontent.com/media/congltk1234/ICT_HM_Fashion_Recommendation/streamlit_deploy/input.jpg')
         my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
         try: 
             if my_upload is not None:
@@ -163,58 +170,51 @@ def main():
             check = np.array(image_caption)
             if check.shape[2] > 3:
                 image_caption = check[...,:3]
+
             caption = generate_caption(image_caption,new_model)
-            # st.sidebar.markdown(f':memo: :red[**Generate Description:**]')
-            # st.sidebar.write(caption)
             caption = st.sidebar.text_input(':memo: :red[**Generate Description:**]', caption)
             st.sidebar.image(image_caption)
             
             caption = sbert_model.encode(caption)
             caption = np.expand_dims(caption, axis=0)
-            text_similarity, describe_recommend = get_best_similiarity(sentence_embeddings , caption , best_n)  
-            distances, img_recommend = compute_distances_fromPath(img, image_model, image_embeddings)
 
-            with st.container():     
-                    container = st.expander(':tshirt: :red[**Similar items based on Image embeddings**]', expanded =True)
-                    with container:
-                        cols = st.columns(6)
-                        cols[0].write('###### Similarity Score')
-                        cols[0].caption(model_descs[0])
-                        for idx, col, score in zip(img_recommend[:5], cols[1:], distances[:5]):
-                            with col:
-                                st.caption('{:.2f}'.format(score))
-                                image = 'https://media.githubusercontent.com/media/congltk1234/HM_images/main/'+items.iloc[idx].image
-                                image = Image.open(io.BytesIO(requests.get(image).content))
-                                st.image(image, use_column_width=True)
-                                item_url = url(items.iloc[idx].article_id)
-                                st.write(f"**[{items.iloc[idx].prod_name}]({item_url})**")
-                                price =items.iloc[idx].price*1000 
-                                st.code(f'''{items.iloc[idx].product_type_name}\n💲{price:.3g}''')
-            with st.container():     
-                    container = st.expander(':left_speech_bubble: :red[**Similar items based on Text embeddings**]', expanded =True)
-                    with container:
-                        cols = st.columns(6)
-                        cols[0].write('###### Similarity Score')
-                        cols[0].caption(model_descs[1])
-                        for idx, col, score in zip(describe_recommend[:5], cols[1:], text_similarity[:5]):
-                            with col:
-                                st.caption('{:.2f}'.format(score))
-                                image = 'https://media.githubusercontent.com/media/congltk1234/HM_images/main/'+items.iloc[idx].image
-                                image = Image.open(io.BytesIO(requests.get(image).content))
-                                st.image(image, use_column_width=True)
-                                item_url = url(items.iloc[idx].article_id)
-                                st.write(f"**[{items.iloc[idx].prod_name}]({item_url})**")
-                                price =items.iloc[idx].price*1000 
-                                st.code(f'''{items.iloc[idx].product_type_name}\n💲{price:.3g}''')
-                                st.caption(f':memo: :red[Description:] \n {items.iloc[idx].detail_desc}')
+            distances, img_recommend = compute_distances_fromPath(img, image_model, image_embeddings)
+            text_similarity, describe_recommend = get_best_similiarity(sentence_embeddings , caption , best_n)  
+                    
+            recommends = {'methods' : [':tshirt: :red[**Similar items based on Image embeddings**]', ':left_speech_bubble: :red[**Similar items based on Text embeddings**]'],
+                          'list_items' : [img_recommend,describe_recommend],
+                          'scores': [distances, text_similarity],
+                          'describes': model_descs,}              
+            
+            for method, list_item, scores, describe in zip(recommends['methods'], recommends['list_items'], recommends['scores'], recommends['describes']):
+                with st.container():     
+                        container = st.expander(method, expanded =True)
+                        with container:
+                            cols = st.columns(6)
+                            cols[0].write('###### Similarity Score')
+                            cols[0].caption(describe)
+                            for idx, col, score in zip(list_item[:5], cols[1:], scores[:5]):
+                                with col:
+                                    st.caption('{:.2f}'.format(score))
+                                    image = 'https://media.githubusercontent.com/media/congltk1234/HM_images/main/'+items.iloc[idx].image
+                                    image = Image.open(io.BytesIO(requests.get(image).content))
+                                    st.image(image, use_column_width=True)
+                                    item_url = url(items.iloc[idx].article_id)
+                                    st.write(f"**[{items.iloc[idx].prod_name}]({item_url})**")
+                                    price =items.iloc[idx].price*1000 
+                                    st.code(f'''{items.iloc[idx].product_type_name}\n💲{price:.3g}''')
         except:
             pass
+
 #########################################################################################
+################ Sector 2: Recommend based-on History purchased #########################
 #########################################################################################
+
     if page_selection == page_options[1]:
         customers_rcmnds = pd.read_csv('data/customers_rcmnds.csv')
         customers = customers_rcmnds.customer_id.unique()   
         get_item = st.sidebar.button('Get Random Customer')
+        get_item = True
         if get_item:
             rand_customer = np.random.choice(customers)
             st.sidebar.markdown(f':memo: :red[**Customer ID:**]')
@@ -224,40 +224,24 @@ def main():
             customer_data = customers_rcmnds[customers_rcmnds.customer_id == rand_customer]
             global customer_history
             customer_history = np.array(eval(customer_data.article_id.iloc[0]))
-            apriori = np.array(eval(customer_data.apriori.iloc[0])).astype(int)
-            uucf = np.array(eval(customer_data.uucf.iloc[0])).astype(int)
 
+            apriori = np.array(eval(customer_data.apriori.iloc[0])).astype(int)
+            
+            uucf = np.array(eval(customer_data.uucf.iloc[0])).astype(int)
             uucf = np.setdiff1d(uucf, customer_history)
-            uucf = np.concatenate((uucf, customer_history), axis=0)
+            uucf = np.concatenate((uucf, customer_history[::-1]), axis=0)
 
             splits = [customer_history[i:i+3] for i in range(0, len(customer_history), 3)]
             for split in splits:
                 with st.sidebar.container():
                     cols = st.columns(3)
                     for item, col in zip(split, cols):
-                        image = items[items['article_id']==item]['image'].values[0]
-                        image = 'https://media.githubusercontent.com/media/congltk1234/HM_images/main/'+image
-                        image = Image.open(io.BytesIO(requests.get(image).content))
-                        image = image.resize((100, 150))
-                        with col:
-                            st.image(image, use_column_width=True)
-                            name = items[items['article_id']==item]['prod_name'].values[0]
-                            item_url = url(item)
-                            st.write(f"**[{name}]({item_url})**")
-                            group = items[items['article_id']==item]['product_type_name'].values[0]
-                            price = items[items['article_id']==item]['price'].values[0]*1000
-                            st.code(f'''{group}\n💲{price:.3g}''')
-
-
-            with st.container():     
-                    container = st.expander(':red[**Association Rules: Apriori Algorithm**]', expanded =True)
-                    with container:
-                        cols = st.columns(6)
-                        for item, col in zip(apriori[:6], cols):
+                        try:
+                            image = items[items['article_id']==item]['image'].values[0]
+                            image = 'https://media.githubusercontent.com/media/congltk1234/HM_images/main/'+image
+                            image = Image.open(io.BytesIO(requests.get(image).content))
+                            image = image.resize((100, 150))
                             with col:
-                                image = items[items['article_id']==item]['image'].values[0]
-                                image = 'https://media.githubusercontent.com/media/congltk1234/HM_images/main/'+image
-                                image = Image.open(io.BytesIO(requests.get(image).content))
                                 st.image(image, use_column_width=True)
                                 name = items[items['article_id']==item]['prod_name'].values[0]
                                 item_url = url(item)
@@ -265,22 +249,76 @@ def main():
                                 group = items[items['article_id']==item]['product_type_name'].values[0]
                                 price = items[items['article_id']==item]['price'].values[0]*1000
                                 st.code(f'''{group}\n💲{price:.3g}''')
-            with st.container():     
-                    container = st.expander(':red[**Collaborative Filtering: User-user**]', expanded =True)
+                        except:
+                            pass
+
+            recommends = {'list_items' : [apriori,uucf],
+                          'methods' : [':red[**Association Rules: Apriori Algorithm**]', ':red[**Collaborative Filtering: User-user**]']}
+               
+            for method, list_item in zip(recommends['methods'], recommends['list_items']):
+                with st.container(): 
+                    container = st.expander(method, expanded =True)
                     with container:
                         cols = st.columns(6)
-                        for item, col in zip(uucf[:6], cols):
+                        for item, col in zip(list_item[:6], cols):
                             with col:
-                                image = items[items['article_id']==item]['image'].values[0]
-                                image = 'https://media.githubusercontent.com/media/congltk1234/HM_images/main/'+image
-                                image = Image.open(io.BytesIO(requests.get(image).content))
-                                st.image(image, use_column_width=True)
-                                name = items[items['article_id']==item]['prod_name'].values[0]
-                                item_url = url(item)
-                                st.write(f"**[{name}]({item_url})**")
-                                group = items[items['article_id']==item]['product_type_name'].values[0]
-                                price = items[items['article_id']==item]['price'].values[0]*1000
-                                st.code(f'''{group}\n💲{price:.3g}''')
+                                try:
+                                    image = items[items['article_id']==item]['image'].values[0]
+                                    image = 'https://media.githubusercontent.com/media/congltk1234/HM_images/main/'+image
+                                    image = Image.open(io.BytesIO(requests.get(image).content))
+                                    st.image(image, use_column_width=True)
+                                    name = items[items['article_id']==item]['prod_name'].values[0]
+                                    item_url = url(item)
+                                    st.write(f"**[{name}]({item_url})**")
+                                    group = items[items['article_id']==item]['product_type_name'].values[0]
+                                    price = items[items['article_id']==item]['price'].values[0]*1000
+                                    st.code(f'''{group}\n💲{price:.3g}''')
+                                except:
+                                    pass
+
+#########################################################################################
+######################## Sector 3: Project Informations #################################
+#########################################################################################
+
+    about_us = { 'names': ['Công Sử', 'Ngân Hồ', 'Trâm Mai', 'Uyên Trần'],
+                'roles' : ['AI/ML💻\nSimilarity Algorithms', 'DA📊\nCustomers Analysis', 'DA📊\nItems Analysis', 'AI/ML💻\nRecommendation Algorithms'],
+                'imgs'  : ['images_256_256/1.png', 'images_256_256/3.png', 'images_256_256/4.png', 'images_256_256/2.png']}
+    if page_selection == page_options[2]:
+        imgURL = "https://logos-world.net/wp-content/uploads/2020/04/HM-Emblem.jpg" 
+        path = 'input.jpg'
+        urllib.request.urlretrieve(imgURL,path)
+        image_caption = Image.open(path).resize((350, 225))
+        st.sidebar.image(image_caption)
+        with st.sidebar.container():
+            with st.columns(3)[1]:
+                st.write("**[HomePage](https://www.hm.com)**")
+
+        with st.expander('💁‍♂️ :red[**ABOUT PROJECT**]'):
+            st.header("H&M Personalized Fashion Recommendations\nProvide product recommendations based on previous purchases")
+            imgURL = "https://www.mrs.org.uk/blog/January%20Pic%203.jpg" 
+            urllib.request.urlretrieve(imgURL,path)
+            image_caption = Image.open(path)     
+            st.image(image_caption, caption='The recommendation techniques used in the project')
+
+        with st.expander('💁‍♂️ :red[**ABOUT US**]'):
+            cols = st.columns(4)
+            for col, name, role, image in zip(cols,about_us['names'],about_us['roles'],about_us['imgs']):
+                with col:
+                    st.subheader(name)
+                    image = Image.open(image).resize((256, 256))
+                    st.image(image, use_column_width=True)
+                    st.code('HueUni\nRole :' + role)
+
+        with st.expander('ℹ️ **DATASOURCE**',expanded =True):
+                st.code('''                     
+                @misc{h-and-m-personalized-fashion-recommendations,
+                    author = {Carlos García Ling, ElizabethHMGroup, FridaRim, inversion, Jaime Ferrando, Maggie, neuraloverflow, xlsrln},
+                    title = {H&M Personalized Fashion Recommendations},
+                    publisher = {Kaggle},
+                    year = {2022},
+                    url = {"https://kaggle.com/competitions/h-and-m-personalized-fashion-recommendations"}
+                }''', language='python')
+
+
 if __name__ == '__main__':
     main()
-
